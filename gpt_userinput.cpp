a2cd8076dea9b1e7b726f21d2731c18d543936f1
Mohammd Shihab Hossain <68942280+Zul-Qarnain@users.noreply.github.com>
2025-07-24 10:36:54 +0600

    Create gpt_userinput.cpp

---
+++ gpt_userinput.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    // Copy data to temp arrays
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+    // Merge the temp arrays back
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+
+    // Copy remaining elements
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int size;
+    
+    // Get array size from user
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    
+    // Validate size
+    if (size <= 0) {
+        cout << "Invalid array size!" << endl;
+        return 1;
+    }
+
+    // Create array with user-specified size
+    int arr[size];
+
+    // Get array elements from user
+    cout << "Enter " << size << " elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    // Sort the array
+    mergeSort(arr, 0, size - 1);
+
+    // Print sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
